3791761988d79b93a5bfbbb76f6b468c38ed5d30
Kameshwaran <kamesh@Kameshs-MacBook-Pro.local>
2018-06-19 19:47:07 +0530

    Working boilerplate

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Index = () => (
+  <div>
+    Hello React!
+  </div>
+);
+
+ReactDOM.render(<Index />, document.getElementById('index'));
