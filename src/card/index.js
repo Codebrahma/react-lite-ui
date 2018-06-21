211346e5010d57ef9910dec786cf599eff264dd0
Arjun <snarjun92@gmail.com>
2018-06-21 19:42:41 +0530

    base card

---
+++ src/card/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { themr } from 'react-css-themr';
+import classnames from 'classnames';
+// import { CARD } from '../identifiers';
+import defaultTheme from './defaultTheme.scss';
+
+const Card = ({ children, className, raised, theme }) => {
+  const classes = classnames(theme.card, {
+    [theme.raised]: raised,
+  }, className);
+
+  return (
+    <div data-react-toolbox="card" className={classes}>
+      {children}
+    </div>
+  );
+};
+
+export default themr('CBCard', defaultTheme)(Card);
