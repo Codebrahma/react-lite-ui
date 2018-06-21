ed88439f9868980430c2b85e23aafd22b9a46bf5
prasanna1211 <prasanna@codebrahma.com>
2018-06-22 00:25:00 +0530

    modified documentation component

--- docs/client/components/PlaygroundPage/PlaygroundWithPreview/index.js
+++ docs/client/components/PlaygroundPage/PlaygroundWithPreview/index.js
@@ -1,11 +1,45 @@
 import React from 'react';
-
+import Preview from './Preview';
+import Playground from './Playground';
 import styles from './styles.scss';
 
-const PlaygroundWithPreview = () => (
-  <div className={styles.playground}>
-    PlaygroundWithPreview
-  </div>
-);
+const defaultCode = `
+  const Example = (
+    <div>
+      Example Component
+    </div>
+  );
+`;
+
+class PlaygroundWithPreview extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const {
+      activeComponent,
+    } = this.props;
+    
+    return (
+      <div className={styles['playground-preview']}>
+        <div className="header">
+          PlaygroundWithPreview
+        </div>
+        
+        <div>
+          <Playground
+            code={defaultCode}
+          />
+          <Preview
+            
+          />
+        </div>
+        {activeComponent}
+      </div>
+    );
+  }
+}
 
 export default PlaygroundWithPreview;
