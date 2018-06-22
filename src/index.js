9872f98e49bd0aa49221e0b99e85c94091ec9f08
Arjun <snarjun92@gmail.com>
2018-06-22 10:54:38 +0530

    more props added

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Card from './card';
+import customTheme from './customcard.scss';
 
 const Index = () => (
   <div>
-    <Card>
+    <Card
+      header='Card String header'
+      wrapContent
+      theme={customTheme}>
       <span>Card Content</span>
       <span>Card Content</span>
-
       <span>Card Content</span>
-      
     </Card>
   </div>
 );
