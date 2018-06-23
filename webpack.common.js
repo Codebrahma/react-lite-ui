44908fb44e48a410eb4cd8f3732c1af21d5b899d
Kameshwaran <kamesh@codebrahma.com>
2018-06-23 13:20:21 +0530

    Removed path

--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const path = require('path');
+
 const devMode = process.env.NODE_ENV !== 'production';
 const htmlWebpackPlugin = new HtmlWebPackPlugin({
   template: './src/index.html',
