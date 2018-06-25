7497f9560b3224201af87fc56a0da29f9f830fae
Kameshwaran <kamesh@codebrahma.com>
2018-06-25 15:40:36 +0530

    Updated server.js to render static assets in prod env

--- docs/server.js
+++ docs/server.js
@@ -5,26 +5,28 @@ const config = require('./webpack.config.development');
 const logger = require('express-logger');
 
 const app = express();
-const compiler = webpack(config);
 
-app.use(require('webpack-dev-middleware')(compiler, {
-  noInfo: true,
-  publicPath: config.output.publicPath,
-  stats: {
-    colors: true
-  }
-}));
-
-app.use(logger({ path: `./log/${process.env.NODE_ENV}.log` }));
-
-// Run webpack middleware only in dev environment
 if (process.env.NODE_ENV !== 'production') {
+  // Run webpack middleware only in dev environment
+  const compiler = webpack(config);
+  app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath,
+    stats: {
+      colors: true
+    }
+  }));
   app.use(require('webpack-hot-middleware')(compiler));
+} else {
+  // Use build folder for serving static assets in prod environment
+  app.use(express.static('build'));
 }
 
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
+
+app.use(logger({ path: `./log/${process.env.NODE_ENV}.log` }));
 
 app.listen(8001, '0.0.0.0', function (err) {
   if (err) {
