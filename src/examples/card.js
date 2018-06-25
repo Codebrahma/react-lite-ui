a91bd85f8e3d6b880aad609828322b9d7871335b
Kameshwaran <kamesh@codebrahma.com>
2018-06-25 16:41:41 +0530

    Added examples

---
+++ src/examples/card.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Card from './../card';
+
+const CardExample = () => (
+  <div>
+    <Card
+      header="Card String header"
+      wrapContent
+    >
+      <div>Hello world!</div>
+      <div>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
+    </Card>
+  </div>
+);
+
+export default CardExample;
