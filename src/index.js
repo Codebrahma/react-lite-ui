af0ab66b144ffeb77ec06fd611ae1fd4d2a95bc5
prasanna1211 <prasanna@codebrahma.com>
2018-07-25 23:05:42 +0530

    made 5 components with single theme dependent styles

--- src/index.js
+++ src/index.js
@@ -1,46 +1,14 @@
-// import Button from './button';
-// import Card from './card';
-// import Checkbox from './checkbox';
-// import CheckboxGroup from './checkboxGroup';
-// import Toggle from './toggle';
-
-// export default {
-//   Button,
-//   Card,
-//   Checkbox,
-//   CheckboxGroup,
-//   Toggle,
-// };
-
-import React from 'react';
-import ReactDOM from 'react-dom';
+import Button from './button';
+import Card from './card';
+import Checkbox from './checkbox';
+import CheckboxGroup from './checkboxGroup';
 import Toggle from './toggle';
-import themes from './theme.scss';
 
-class ToggleDisplay extends React.Component {
-  constructor(props) {
-    super(props);
+export default {
+  Button,
+  Card,
+  Checkbox,
+  CheckboxGroup,
+  Toggle,
+};
 
-    this.state = {
-      toggled: false,
-    };
-  }
-
-  handleToggle = () => {
-    this.setState({
-      toggled: !this.state.toggled,
-    });
-  }
-
-  render() {
-    return (
-      <Toggle
-        toggled={this.state.toggled}
-        onClick={this.handleToggle}
-        theme={themes}
-      />
-    );
-  }
-}
-
-ReactDOM.render(<ToggleDisplay />, document.getElementById('index'));
