8f55d05edc82cd0ec7f4fa898fa8078a6c110125
prasanna1211 <prasanna@codebrahma.com>
2018-07-25 20:25:26 +0530

    added toggle

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
-import Button from './button';
-import Card from './card';
-import Checkbox from './checkbox';
-import CheckboxGroup from './checkboxGroup';
+// import Button from './button';
+// import Card from './card';
+// import Checkbox from './checkbox';
+// import CheckboxGroup from './checkboxGroup';
 
-export default {
-  Button,
-  Card,
-  Checkbox,
-  CheckboxGroup,
-};
+// export default {
+//   Button,
+//   Card,
+//   Checkbox,
+//   CheckboxGroup,
+// };
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Toggle from './toggle';
+
+class ToggleDisplay extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      toggled: false,
+    };
+  }
+  
+  handleToggle = () => {
+    this.setState({
+      toggled: !this.state.toggled,
+    });
+  }
+
+  render() {
+    return (
+      <Toggle
+        toggled={this.state.toggled}
+        onClick={this.handleToggle}
+      />
+    );
+  }
+}
+
+ReactDOM.render(<ToggleDisplay />, document.getElementById('index'));
