49f23c58abfa30e7347d5ac393658ef880b457bb
prasanna1211 <prasanna@codebrahma.com>
2018-07-26 12:22:04 +0530

    Added lite ui

--- docs/client/components/common/DefaultCode/index.js
+++ docs/client/components/common/DefaultCode/index.js
@@ -3,3 +3,4 @@ export { defaultCode as ButtonDefaultCode } from './Button';
 export { defaultCode as CheckboxDefaultCode } from './Checkbox';
 export { defaultCode as CheckboxGroupDefaultCode } from './CheckboxGroup';
 export { defaultCode as ToggleDefaultCode } from './Toggle';
+export { defaultCode as RadioButtonGroupDefaultCode } from './RadioButtonGroup';
--- src/index.js
+++ src/index.js
@@ -1,32 +1,34 @@
-// import Button from './button';
-// import Card from './card';
-// import Checkbox from './checkbox';
-// import CheckboxGroup from './checkboxGroup';
-// import Toggle from './toggle';
-
-// export default {
-//   Button,
-//   Card,
-//   Checkbox,
-//   CheckboxGroup,
-//   Toggle,
-// };
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-
+import Button from './button';
+import Card from './card';
+import Checkbox from './checkbox';
+import CheckboxGroup from './checkboxGroup';
+import Toggle from './toggle';
 import RadioButtonGroup from './radioButtonGroup';
-import theme from './theme.scss';
 
-const options = [{ label: 'Alpha' }, { label: 'Beta' }, { label: 'Zheta' }]
-const RadioDisplay = () => (
-  <div>
-    <RadioButtonGroup
-      theme={theme}
-      options={options}
-      inline
-    />
-  </div>
-);
+export default {
+  Button,
+  Card,
+  Checkbox,
+  CheckboxGroup,
+  RadioButtonGroup,
+  Toggle,
+};
 
-ReactDOM.render(<RadioDisplay />, document.getElementById('index'));
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// import RadioButtonGroup from './radioButtonGroup';
+// import theme from './theme.scss';
+
+// const options = [{ label: 'Alpha' }, { label: 'Beta' }, { label: 'Zheta' }]
+// const RadioDisplay = () => (
+//   <div>
+//     <RadioButtonGroup
+//       theme={theme}
+//       options={options}
+//       inline
+//     />
+//   </div>
+// );
+
+// ReactDOM.render(<RadioDisplay />, document.getElementById('index'));
