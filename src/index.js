362566da996ce5fecedd58828e967c9b953c0deb
Arjun <snarjun92@gmail.com>
2018-07-27 17:23:34 +0530

    resolve merge conflicts

--- src/index.js
+++ src/index.js
@@ -7,21 +7,8 @@ import CheckboxGroup from './checkboxGroup';
 import Toggle from './toggle';
 import RadioButtonGroup from './radioButtonGroup';
 import TextInput from './textInput';
-
-export default {
-  Button,
-  Card,
-  Checkbox,
-  CheckboxGroup,
-  RadioButtonGroup,
-  Toggle,
-  TextInput
-};
 import Snackbar from './snackbar';
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-
 export default {
   Button,
   Card,
@@ -29,45 +16,6 @@ export default {
   CheckboxGroup,
   RadioButtonGroup,
   Toggle,
+  TextInput,
   Snackbar,
 };
-
-import theme from './theme.scss';
-
-class SnackDisplay extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showSnackbar: false,
-      showTopSnackbar: false,
-      showIndefiniteSnackbar: false,
-    }
-  }
-
-  openBottomSnackbar = () => {
-    this.setState({
-      showSnackbar: !this.state.showSnackbar
-    });
-  }
-
-  openTopSnackbar = () => {
-    this.setState({
-      showTopSnackbar: !this.state.showTopSnackbar
-    });
-  }
-
-  openIndefiniteSnackbar = () => {
-    this.setState({
-      showIndefiniteSnackbar: !this.state.showIndefiniteSnackbar
-    });
-  }
-  
-  
-  handleSnackClose = () => {
-    this.setState({
-      showSnackbar: false,
-      showTopSnackbar: false,
-    });
-  }
-
-// ReactDOM.render(<RadioDisplay />, document.getElementById('index'));
