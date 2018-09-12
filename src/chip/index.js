c78733a7588a4dba2e5697f7348e60bd5bf52bb1
sai <kskprasad96@gmail.com>
2018-09-12 11:08:25 +0530

    lint fixes

--- src/chip/index.js
+++ src/chip/index.js
@@ -4,15 +4,15 @@ import { themr } from 'react-css-themr';
 import classnames from 'classnames';
 import defaultTheme from './theme.scss';
 
-const DeleteIcon = ({classes, handleClick}) => {
+const DeleteIcon = ({ classes, handleClick }) => {
   function handleDeleteClick(e) {
     e.stopPropagation();
     return handleClick(e);
   }
   return (
     <button onClick={handleDeleteClick} className={classes}>X</button>
-  )
-}
+  );
+};
 
 const Chip = ({
   theme,
@@ -23,45 +23,50 @@ const Chip = ({
   outlined,
   handleDelete,
   handleChipClick,
+  className,
   ...props
 }) => {
   const Element = !handleChipClick ? 'div' : 'a';
 
-  const classes = classnames(theme.chip, theme[size],
+  const classes = classnames(
+    theme.chip,
+    theme[size],
     theme[outlined ? `${color}Outlined` : color],
-    {[theme.clickable]: Element === 'a'}
-  )
+    { [theme.clickable]: Element === 'a' },
+    className,
+  );
   /* Avatar related block */
   let avatar = null;
-  if(avatarElement && React.isValidElement(avatarElement)) {
+  if (avatarElement && React.isValidElement(avatarElement)) {
     avatar = React.cloneElement(avatarElement, {
-      className: classnames(avatarElement.props.className,
-        theme[`${size}Avatar`]
-      )
-    })
-  } else if (typeof avatarElement === 'string' ) {
-    avatar = <img src={avatarElement} alt='A' className={classnames(theme[`${size}Avatar`])}/>
+      className: classnames(
+        avatarElement.props.className,
+        theme[`${size}Avatar`],
+      ),
+    });
+  } else if (typeof avatarElement === 'string') {
+    avatar = <img src={avatarElement} alt="A" className={classnames(theme[`${size}Avatar`])} />;
   }
 
   /* DeleteIcon related block */
   let deleteIcon = null;
-  if(handleDelete) {
-    const customClasses = classnames(theme.deleteIcon,
-        theme[size && `${size}Delete`],
-        theme[outlined ? color : `${color}Outlined`]
-    )
-    deleteIcon =  <DeleteIcon classes={customClasses} handleClick={handleDelete}/>
+  if (handleDelete) {
+    const customClasses = classnames(
+      theme.deleteIcon,
+      theme[size && `${size}Delete`],
+      theme[outlined ? color : `${color}Outlined`],
+    );
+    deleteIcon = <DeleteIcon classes={customClasses} handleClick={handleDelete} />;
   }
 
-
   return (
-    <Element href='#' className={classes} {...props} onClick={Element === 'a' ? handleChipClick : ()=> {}}>
+    <Element href="#" className={classes} {...props} onClick={Element === 'a' ? handleChipClick : () => {}}>
       { avatar }
       <span className={theme.label}>{ label }</span>
       { deleteIcon }
     </Element>
-  )
-}
+  );
+};
 
 Chip.propTypes = {
   label: PropTypes.string,
@@ -69,12 +74,20 @@ Chip.propTypes = {
   size: PropTypes.string,
   outlined: PropTypes.bool,
   handleDelete: PropTypes.func,
+  handleChipClick: PropTypes.func,
+  avatar: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+  className: PropTypes.string,
 };
 
 Chip.defaultProps = {
   label: 'default chip',
   color: 'secondary',
   size: 'medium',
+  outlined: false,
+  handleDelete: null,
+  handleChipClick: null,
+  avatar: null,
+  className: '',
 };
 
 export default themr('CBChips', defaultTheme)(Chip);
