2eb94552e04632cded450b6e6eda0125acf6640b
sai <kskprasad96@gmail.com>
2018-09-27 19:08:11 +0530

    added test cases for avatar

--- src/avatar/index.test.js
+++ src/avatar/index.test.js
@@ -1,12 +1,48 @@
 import React from 'react';
 import { assert } from 'chai';
-import { mount } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import Avatar from './index';
 
 /* eslint-disable no-undef */
-describe('<Avatar />', () => {
+describe('Render Avatar Component', () => {
+  let wrapper;
   it('Should Render Avatar', () => {
-    const wrapper = mount(<Avatar />);
-    assert.strictEqual(wrapper.name(), 'ThemedAvatar');
+    wrapper = shallow(<Avatar />);
+    assert.strictEqual(wrapper.name(), 'Avatar');
   });
+
+  describe('Render image avatar', () => {
+
+    beforeEach(() => {
+      wrapper = mount(
+        <Avatar 
+          image="https://someurl.com/someimage"
+          className="avatar-class"
+        />
+      )
+    });
+
+    afterEach(() => {
+      wrapper.unmount();
+    });
+    
+    it('image prop must be string', () => {
+      assert.strictEqual(wrapper.prop('image'), 'https://someurl.com/someimage');
+    });
+    it('should accept className prop', () => {
+      assert.strictEqual(wrapper.prop('className'), 'avatar-class');
+    })
+  });
+
+  describe('Render text avatar', () => {
+    it('should render avtar with a text', () => {
+      const wrapper = shallow(
+        <Avatar>
+          CB
+        </Avatar>
+      );
+      assert.strictEqual(wrapper.childAt(0).text(), 'CB');
+    });
+  });
+
 });
