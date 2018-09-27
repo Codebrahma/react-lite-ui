2ecee3079eff983af094b99a8c15d85b1c83b6ba
sai <kskprasad96@gmail.com>
2018-09-27 15:13:46 +0530

    jsdom-global added

--- src/avatar/index.test.js
+++ src/avatar/index.test.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { assert } from 'chai';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import Avatar from './index';
 
 /* eslint-disable no-undef */
 describe('<Avatar />', () => {
   it('Should Render Avatar', () => {
-    const wrapper = shallow(<Avatar />);
-    assert.strictEqual(wrapper.name(), 'Avatar');
+    const wrapper = mount(<Avatar />);
+    assert.strictEqual(wrapper.name(), 'ThemedAvatar');
   });
 });
