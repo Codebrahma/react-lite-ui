6bf60589930b457cdaafa51f675912c792996815
sai <kskprasad96@gmail.com>
2018-09-28 11:02:59 +0530

    tooltip render test added

---
+++ src/tooltip/index.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { expect } from 'chai';
+import { mount, shallow } from 'enzyme';
+import Tooltip from './index';
+
+/* eslint-disable no-undef */
+describe('Render Tooltip Component', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(
+      <Tooltip
+        tooltipText='Some tip text'
+        top
+      >
+        Tooltip Child
+      </Tooltip>
+    );
+  });
+
+  afterEach(() => {
+    wrapper.unmount();
+  });
+
+  it('Should render Tooltip component', () => {
+    expect(wrapper.name(), 'Tooltip');
+  });
+
+  it('Should render children', () => {
+    const wrappedComponent = shallow(<Tooltip tooltipText="text">Child</Tooltip>)
+    expect(wrappedComponent.children()).to.have.length(1);
+  });
+
+});
