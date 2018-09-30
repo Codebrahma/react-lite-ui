443f8adbfc53d936b83b04be3571c7a88359cf1d
sai <kskprasad96@gmail.com>
2018-09-30 19:42:20 +0530

    toggle docs added

--- src/toggle/index.js
+++ src/toggle/index.js
@@ -22,6 +22,7 @@ const Toggle = ({
       <input
         type="checkbox"
         onClick={() => onClick(label, value)}
+        name={label}
         {...props}
       />
       <span className="toggle-item" />
