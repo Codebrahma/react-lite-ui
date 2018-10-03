890e54c8bfb7a9b78f7f9d502eb8de9a4ac8c057
rishichawda <chawda.rishii@gmail.com>
2018-10-03 18:38:05 +0530

    test(select): add render tests

--- src/select/index.js
+++ src/select/index.js
@@ -73,47 +73,53 @@ class Select extends Component {
   // Helper function to render options inside the dropdown.
   renderOptions = (options) => {
     const { theme } = this.props;
-    return options
-      .map(option => (
-        /* eslint-disable jsx-a11y/click-events-have-key-events */
-        /* eslint-disable jsx-a11y/no-static-element-interactions */
-        <div
-          className={theme.option}
-          onClick={() => this.handleSelect(option)}
-          key={option.label}
-        >
-          {option.label}
-        </div>
-      ));
+    return options.map(option => (
+      /* eslint-disable jsx-a11y/click-events-have-key-events */
+      /* eslint-disable jsx-a11y/no-static-element-interactions */
+      <span
+        className={theme.option}
+        onClick={() => this.handleSelect(option)}
+        key={option.label}
+      >
+        {option.label}
+      </span>
+    ));
   };
 
   render() {
     const { options, theme, className } = this.props;
     const { selected, open } = this.state;
     const classes = cx(theme.select, className);
-    const menuclass = cx(theme.menu, open ? theme.show : theme.hide);
+    const menuclass = cx(theme.menu, theme.show);
     const arrowclass = cx(theme.arrow, open ? theme.up : theme.down);
     return (
       <div className={classes}>
         <div
+          id="select"
           className={theme.selectInput}
           onClick={this.toggleMenu}
           onBlur={this.hideMenu}
           /* eslint-disable jsx-a11y/no-noninteractive-tabindex */
           tabIndex={0}
         >
-          <input className={theme.selected} type="text" value={selected.label} disabled />
+          <input
+            className={theme.selected}
+            type="text"
+            value={selected.label}
+            disabled
+          />
           <div className={arrowclass} />
         </div>
-        <div
-          className={menuclass}
-          onMouseEnter={() => this.blockOnBlur(true)}
-          onMouseLeave={() => this.blockOnBlur(false)}
-        >
-          <div className={cx(theme.options_wrapper)}>
+        {open && (
+          <div
+            id="select-dropdown"
+            className={menuclass}
+            onMouseEnter={() => this.blockOnBlur(true)}
+            onMouseLeave={() => this.blockOnBlur(false)}
+          >
             {this.renderOptions(options)}
           </div>
-        </div>
+        )}
       </div>
     );
   }
