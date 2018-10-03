573cb1e35bebb5d52bfcb499baa696da2832d469
rishichawda <chawda.rishii@gmail.com>
2018-10-03 11:39:16 +0530

    fix(playground page): create individual doc link for components - issue #8

--- docs/client/components/PlaygroundPage/index.js
+++ docs/client/components/PlaygroundPage/index.js
@@ -1,38 +1,53 @@
-import React from 'react';
-import ComponentBar from './ComponentBar';
-import PlaygroundWithPreview from './PlaygroundWithPreview';
-import Documentation from './Documentation';
+import React from "react";
+import ComponentBar from "./ComponentBar";
+import PlaygroundWithPreview from "./PlaygroundWithPreview";
+import Documentation from "./Documentation";
+import { componentList } from '../common/componentList';
+import Card from "../../../../src/card";
+import CardReadme from "../../../../src/card/readMe.md";
 
-import Card from '../../../../src/card';
-import CardReadme from '../../../../src/card/readMe.md';
-
-import { CardDefaultCode } from '../common/DefaultCode';
-
+import { CardDefaultCode } from "../common/DefaultCode";
 
 class PlaygroundPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       isDocumentationOn: false,
-      activeComponent: 'Card',
+      activeComponent: "Card",
       defaultCode: CardDefaultCode,
-      activeDocs: CardReadme,
+      activeDocs: CardReadme
     };
     this.expandDocumentation = this.expandDocumentation.bind(this);
     this.handleClickComponent = this.handleClickComponent.bind(this);
   }
 
+  componentDidMount() {
+    const url = window.location.href;
+    const currentRoute = url.substring(url.lastIndexOf('/')+1);
+    const queryComponent = componentList.filter(component => component.name.toLowerCase() === currentRoute)[0];
+    this.setState({
+      activeComponent: queryComponent.name,
+      defaultCode: queryComponent.defaultCode,
+      activeDocs: queryComponent.docs,
+    });
+  }
+
   expandDocumentation() {
     this.setState({
-      isDocumentationOn: !this.state.isDocumentationOn,
+      isDocumentationOn: !this.state.isDocumentationOn
     });
   }
 
   handleClickComponent(activeComponent, defaultCode, activeDocs) {
+    window.history.pushState(
+      "",
+      activeComponent,
+      `/react-lite-ui/playground/${activeComponent.toLowerCase()}`
+    );
     this.setState({
       activeComponent,
       defaultCode,
-      activeDocs,
+      activeDocs
     });
   }
 
@@ -49,14 +64,12 @@ class PlaygroundPage extends React.Component {
             expandDocumentation={this.expandDocumentation}
             isDocumentationOn={this.state.isDocumentationOn}
           />
-          {
-            this.state.isDocumentationOn && (
-              <Documentation
-                activeDocs={this.state.activeDocs}
-                isDocumentationOn={this.state.isDocumentationOn}
-              />
-            )
-          }
+          {this.state.isDocumentationOn && (
+            <Documentation
+              activeDocs={this.state.activeDocs}
+              isDocumentationOn={this.state.isDocumentationOn}
+            />
+          )}
         </ComponentBar>
       </div>
     );
