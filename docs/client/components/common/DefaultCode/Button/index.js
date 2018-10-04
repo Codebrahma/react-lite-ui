74dae632ba200b217e3e6a0ef1395d9070527a21
rishichawda <chawda.rishii@gmail.com>
2018-10-04 11:30:23 +0530

    refactor(defaultCode/button): refactor demo code

--- docs/client/components/common/DefaultCode/Button/index.js
+++ docs/client/components/common/DefaultCode/Button/index.js
@@ -1,47 +1,53 @@
 export const defaultCode = `
 class Demo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.style = {
+      buttonContainer: {
+        display: "grid",
+        gridTemplateColumns: "auto auto auto",
+        gridGap: 20
+      }
+    };
+  }
   render() {
     return (
       <div>
         <PreviewBlock header="Styled Buttons">
-        <div style={{display: 'grid',
-        gridTemplateColumns: 'auto auto auto', gridGap: 20}} >
-          <Button>Default</Button>
-          <Button type="primary">Primary</Button>
-          <Button type="secondary">Secondary</Button>
-          <Button type="success">Success</Button>
-          <Button type="danger">Danger</Button>
-          <Button type="warning">Warning</Button>
-          <Button type="info">Info</Button>
-          <Button type="light">Light</Button>
-          <Button type="dark">Dark</Button>
+          <div style={this.style.buttonContainer}>
+            <Button>Default</Button>
+            <Button type="primary">Primary</Button>
+            <Button type="secondary">Secondary</Button>
+            <Button type="success">Success</Button>
+            <Button type="danger">Danger</Button>
+            <Button type="warning">Warning</Button>
+            <Button type="info">Info</Button>
+            <Button type="light">Light</Button>
+            <Button type="dark">Dark</Button>
           </div>
         </PreviewBlock>
         <PreviewBlock header="Sizable Buttons">
-        <div style={{display: 'grid',
-        gridTemplateColumns: 'auto auto auto', gridGap: 20, alignItems: 'center'}} >
-          <Button 
-            type="primary"
-            size="small"
+          <div
+            style={{
+              display: "grid",
+              gridTemplateColumns: "auto auto auto",
+              gridGap: 20,
+              alignItems: "center"
+            }}
           >
-            Small
-          </Button> 
-          <Button
-            type="primary"
-            size="medium"
-          >
-            Medium
-          </Button>
-          <Button 
-            type="primary"
-            size="large"
-          >
-            Large
-          </Button>
+            <Button type="primary" size="small">
+              Small
+            </Button>
+            <Button type="primary" size="medium">
+              Medium
+            </Button>
+            <Button type="primary" size="large">
+              Large
+            </Button>
           </div>
         </PreviewBlock>
       </div>
-    )
+    );
   }
 }
 `;
