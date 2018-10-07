b3438e811ed0f556eb3fea9e5886e2d3933d9ed5
sai <kskprasad96@gmail.com>
2018-10-07 19:29:15 +0530

    docs(navbar): playground updated

--- docs/client/components/common/DefaultCode/Navbar/index.js
+++ docs/client/components/common/DefaultCode/Navbar/index.js
@@ -4,7 +4,6 @@ class Demo extends React.Component {
     super(props);
     this.state = {
       toggled: false,
-      opened: false,
     };
     this.handleToggle = this.handleToggle.bind(this);
     this.handleModal = this.handleModal.bind(this);
@@ -27,24 +26,30 @@ class Demo extends React.Component {
       <svg xmlns="http://www.w3.org/2000/svg" height="20px" id="Layer_1"  version="1.1" viewBox="0 0 32 32" width="20px"><path d="M4,10h24c1.104,0,2-0.896,2-2s-0.896-2-2-2H4C2.896,6,2,6.896,2,8S2.896,10,4,10z M28,14H4c-1.104,0-2,0.896-2,2  s0.896,2,2,2h24c1.104,0,2-0.896,2-2S29.104,14,28,14z M28,22H4c-1.104,0-2,0.896-2,2s0.896,2,2,2h24c1.104,0,2-0.896,2-2  S29.104,22,28,22z"/></svg>
     );
 
+    const NavLink = ({link}) => (
+      <div style={{padding: '0 5px'}}>{link}</div>
+    );
+
     return (
       <div>
         { /* Preview Block-1 */ }
         <PreviewBlock header="Simple Navbar">
-          <Navbar />
+          <Navbar title='navbar'/>
         </PreviewBlock>
         { /* Preview Block-2 */ }
         <PreviewBlock header="Navbar with icon on left">
           <Navbar leftIcon={<MenuIcon />}>
-            <ListItem />
-            <ListItem />
+            <NavLink link='item-1'/>
+            <NavLink link='item-2'/>
+            <NavLink link='item-3'/>
           </Navbar>
         </PreviewBlock>
         { /* Preview Block-3 */ }
         <PreviewBlock header="Navbar with title as image or logo and flat (No box-shadow)">
           <Navbar flat title={<img src="https://picsum.photos/100/30" />} rightIcon={<MenuIcon />}>
-            <ListItem />
-            <ListItem />
+            <NavLink link='item-1'/>
+            <NavLink link='item-2'/>
+            <NavLink link='item-3'/>
           </Navbar>
         </PreviewBlock>
         { /* Preview Block-4 */ }
@@ -57,18 +62,8 @@ class Demo extends React.Component {
             <span style={{ marginLeft: '5px' }}>{ this.state.toggled ? 'Logout' : 'Login' }</span>
           </div>
           <Navbar title="logo" rightIcon={this.state.toggled && <Avatar>c</Avatar>}>
-            <ListItem />
-            <ListItem />
+            <TextInput width="150px" placeholder='search users'/>
           </Navbar>
-        </PreviewBlock>
-        { /* Preview Block-5 */ }
-        <PreviewBlock header='Navbar with Login button on right'>
-          <Navbar title='logo'>
-            <ListItem />
-            <ListItem />
-            <Button onClick={this.handleModal}>Login</Button>
-          </Navbar>
-          <Modal title="You can do some login related stuff here" open={this.state.opened} />
         </PreviewBlock>
       </div>
     )
