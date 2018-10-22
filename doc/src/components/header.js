e1487d77bcf1ff4e7cdb6d43bf1fc526bb770f4f
prasanna1211 <prasanna@codebrahma.com>
2018-10-22 23:54:31 +0530

    :lipstick: added skew + styles

--- doc/src/components/header.js
+++ doc/src/components/header.js
@@ -1,32 +1,11 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import styles from './styles.scss'
 
+import triangle from './triangle.svg';
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+  <div className={styles.navbar}>
+    <img src={triangle} />
   </div>
 )
 
--- doc/src/components/layout.js
+++ doc/src/components/layout.js
@@ -4,7 +4,6 @@ import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 
 import Header from './header'
-import './layout.css'
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -29,14 +28,7 @@ const Layout = ({ children }) => (
           <html lang="en" />
         </Helmet>
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
+        <div>
           {children}
         </div>
       </>
