c7b3c97a7e90744a331616b3cdfb3b085e983e64
rishichawda <chawda.rishii@gmail.com>
2018-10-22 16:56:52 +0530

    ♿️ add left and right controls
    add optional controls that can be turned off through props.

--- docs/client/components/common/DefaultCode/Carousel/index.js
+++ docs/client/components/common/DefaultCode/Carousel/index.js
@@ -3,7 +3,8 @@ class Demo extends React.Component {
     render() {
       const data = [
         "https://images.pexels.com/photos/371633/pexels-photo-371633.jpeg?cs=srgb&dl=wood-landscape-sky-371633.jpg&fm=jpg",
-        "https://hanslodge.com/images/pTqrL9Rec.jpg"
+        "https://hanslodge.com/images/pTqrL9Rec.jpg",
+        "https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?cs=srgb&dl=sea-landscape-nature-248797.jpg&fm=jpg"
       ];
       return (
         <div>
--- src/carousel/index.js
+++ src/carousel/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { themr } from 'react-css-themr';
+import cx from 'classnames';
 
 import CarouselItem from './carouselItem';
 import defaultTheme from './theme.scss';
+import '../globals/fonts.scss';
 
 class Carousel extends React.Component {
   constructor(props) {
@@ -11,20 +13,39 @@ class Carousel extends React.Component {
     this.state = {
       active: 0,
     };
+    this.animation = null;
+    this.animate();
+  }
 
-    setInterval(() => {
+  setActiveItem = (active) => {
+    const { data } = this.props;
+    clearInterval(this.animation);
+    this.setState({
+      active: (data.length + active) % data.length,
+    }, this.animate);
+  };
+
+  animate = () => {
+    const { data, interval } = this.props;
+    this.animation = setInterval(() => {
       this.setState(prevState => ({
-        active: (prevState.active + 1) % props.data.length,
+        active: (prevState.active + 1) % data.length,
       }));
-    }, props.interval);
+    }, interval);
   }
 
   render() {
-    const { theme } = this.props;
+    const { theme, data, controls } = this.props;
     const { active } = this.state;
     return (
       <div className={theme['carousel-container']}>
-        {this.props.data.map((item, index) => (
+        {/* eslint-disable jsx-a11y/click-events-have-key-events */}
+        {/* eslint-disable jsx-a11y/no-static-element-interactions */}
+        {
+          controls &&
+          <i className={cx(theme['left-control'], 'icon-left')} onClick={() => this.setActiveItem(active - 1)} />
+        }
+        {data.map((item, index) => (
           <CarouselItem
             theme={theme}
             active={index === active ? true : undefined}
@@ -33,6 +54,18 @@ class Carousel extends React.Component {
             {index}
           </CarouselItem>
         ))}
+        <div className={theme.dot}>
+          {data.map((_, index) => (
+            <span
+              onClick={() => this.setActiveItem(index)}
+              className={cx({ active: active === index })}
+            />
+          ))}
+        </div>
+        {
+          controls &&
+          <i className={cx(theme['right-control'], 'icon-right')} onClick={() => this.setActiveItem(active + 1)} />
+        }
       </div>
     );
   }
@@ -42,11 +75,13 @@ Carousel.propTypes = {
   data: PropTypes.oneOfType([PropTypes.array]).isRequired,
   theme: PropTypes.oneOfType([PropTypes.object]),
   interval: PropTypes.number,
+  controls: PropTypes.bool,
 };
 
 Carousel.defaultProps = {
   interval: 4000,
   theme: defaultTheme,
+  controls: true,
 };
 
 export default themr('CBCarousel', defaultTheme)(Carousel);
