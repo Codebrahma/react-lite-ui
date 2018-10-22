abe1cb55df28c18834134990a464bb32954c1425
prasanna1211 <prasanna@codebrahma.com>
2018-10-22 23:23:36 +0530

    :sparkles: removed lock

--- doc/src/pages/index.js
+++ doc/src/pages/index.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+import * as components from '../../../src';
 import Layout from '../components/layout'
+
+const {
+  TextInput,
+} = components.default;
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    Site will come here
   </Layout>
 )
 
