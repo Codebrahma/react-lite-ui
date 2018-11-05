f1c09f85f3f56148318942eab7c0776907456e52
rishichawda <chawda.rishii@gmail.com>
2018-11-05 15:03:00 +0530

    ✨  (slider/index.js)-add range slider

--- src/slider/index.js
+++ src/slider/index.js
@@ -13,6 +13,7 @@ class Slider extends React.Component {
     this.sliderTracker = null;
     this.sliderBar = null;
     this.sliderUpperRange = null;
+    this.dragEventWithData = null;
     this.state = {
       // valueMax : used as max value selected in a range slider.
       // and is default store for normal slider max value.
@@ -23,34 +24,102 @@ class Slider extends React.Component {
     };
   }
 
-  dragEvent = (e) => {
-    if (
-      (e.clientX <=
-        (findNode(this.sliderBar).offsetLeft +
-          findNode(this.sliderBar).getBoundingClientRect().width)) &&
-      (e.clientX >= findNode(this.sliderBar).offsetLeft)
-    ) {
-      const width =
-        ((e.clientX - findNode(this.sliderBar).offsetLeft) /
-          findNode(this.sliderBar).getBoundingClientRect().width) *
-        100;
-      findNode(this.sliderTracker).style.width = `${width}%`;
-      this.setState({
-        valueMax: Math.round((width / 100) * this.props.max),
-      }, () => {
-        const { onChange } = this.props;
-        const { valueMin, valueMax } = this.state;
-        onChange(valueMin, valueMax);
-      });
+  dragEvent = (e, left) => {
+    if (this.props.range) {
+      if (left) {
+        const sliderTrackerWidth = findNode(this.sliderTracker).getBoundingClientRect().width;
+        if (
+          e.clientX <=
+            findNode(this.sliderOffset).getBoundingClientRect().width +
+              sliderTrackerWidth +
+              findNode(this.sliderBar).offsetLeft &&
+          e.clientX >= findNode(this.sliderBar).offsetLeft
+        ) {
+          const prevWidth = findNode(this.sliderOffset).getBoundingClientRect()
+            .width;
+          const width =
+            (e.clientX - findNode(this.sliderBar).offsetLeft) /
+            findNode(this.sliderBar).getBoundingClientRect().width;
+          findNode(this.sliderOffset).style.width = `${width * 100}%`;
+          const trackerWidth =
+            (findNode(this.sliderTracker).getBoundingClientRect().width -
+              (e.clientX - findNode(this.sliderBar).offsetLeft - prevWidth)) /
+            findNode(this.sliderBar).getBoundingClientRect().width;
+          findNode(this.sliderTracker).style.width = `${trackerWidth * 100}%`;
+          this.setState(
+            {
+              valueMin: Math.round(width * this.props.max),
+            },
+            () => {
+              const { onChange } = this.props;
+              const { valueMin, valueMax } = this.state;
+              onChange(valueMin, valueMax);
+            },
+          );
+        }
+      } else {
+        const sliderBarWidth = findNode(this.sliderBar).getBoundingClientRect()
+          .width;
+        if (
+          e.clientX <= findNode(this.sliderBar).offsetLeft + sliderBarWidth &&
+          e.clientX >=
+            findNode(this.sliderBar).offsetLeft +
+              findNode(this.sliderOffset).getBoundingClientRect().width
+        ) {
+          const width =
+            ((e.clientX -
+              findNode(this.sliderBar).offsetLeft -
+              findNode(this.sliderOffset).getBoundingClientRect().width) /
+              sliderBarWidth) *
+            100;
+          findNode(this.sliderTracker).style.width = `${width}%`;
+          const maxValue =
+            (e.clientX - findNode(this.sliderBar).offsetLeft) / sliderBarWidth;
+          this.setState(
+            {
+              valueMax: Math.round(maxValue * this.props.max),
+            },
+            () => {
+              const { onChange } = this.props;
+              const { valueMin, valueMax } = this.state;
+              onChange(valueMin, valueMax);
+            },
+          );
+        }
+      }
+    } else {
+      const sliderBarWidth = findNode(this.sliderBar).getBoundingClientRect()
+        .width;
+      if (
+        e.clientX <= findNode(this.sliderBar).offsetLeft + sliderBarWidth &&
+        e.clientX >= findNode(this.sliderBar).offsetLeft
+      ) {
+        const width =
+          ((e.clientX - findNode(this.sliderBar).offsetLeft) / sliderBarWidth) *
+          100;
+        findNode(this.sliderTracker).style.width = `${width}%`;
+        this.setState(
+          {
+            valueMax: Math.round((width / 100) * this.props.max),
+          },
+          () => {
+            const { onChange } = this.props;
+            const { valueMin, valueMax } = this.state;
+            onChange(valueMin, valueMax);
+          },
+        );
+      }
     }
   };
 
-  startDrag = () => {
+  startDrag = (_, left) => {
+    this.dragEventWithData = e => this.dragEvent(e, left);
     document.body.addEventListener('mouseup', this.stopDrag);
-    document.body.addEventListener('mousemove', this.dragEvent);
+    document.body.addEventListener('mousemove', this.dragEventWithData);
   };
+
   stopDrag = () => {
-    document.body.removeEventListener('mousemove', this.dragEvent);
+    document.body.removeEventListener('mousemove', this.dragEventWithData);
   };
 
   render() {
@@ -69,8 +138,12 @@ class Slider extends React.Component {
               this.sliderOffset = ref;
             }}
           >
+            {/* eslint-disable-next-line jsx-a11y/no-static-element-interactions */}
             <span
               className={theme['slider-button']}
+              onMouseDown={(e) => {
+                this.startDrag(e, true);
+              }}
               ref={(ref) => {
                 this.sliderLowerRange = ref;
               }}
@@ -90,7 +163,9 @@ class Slider extends React.Component {
           {/* eslint-disable-next-line jsx-a11y/no-static-element-interactions */}
           <span
             className={theme['slider-button']}
-            onMouseDown={this.startDrag}
+            onMouseDown={(e) => {
+              this.startDrag(e, false);
+            }}
             ref={(ref) => {
               this.sliderUpperRange = ref;
             }}
