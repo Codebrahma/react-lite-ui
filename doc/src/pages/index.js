71369388695f7c87600663cdad9663b1495951b9
rishichawda <chawda.rishii@gmail.com>
2018-11-27 12:05:37 +0530

    💄  (hero)-fix hero section bottom

--- doc/src/pages/index.js
+++ doc/src/pages/index.js
@@ -1,28 +1,23 @@
-import React from 'react'
-import { Link } from 'gatsby'
-
-import * as components from '../../../src';
-import Layout from '../components/layout'
+import React from 'react';
+import Layout from '../components/layout';
 import './index.scss';
 import liteLogo from '../images/match.svg';
-
-const {
-  TextInput,
-} = components.default;
+import LandingMain from '../templates/landing-main';
 
 const IndexPage = () => (
   <Layout>
     <div className="hero-container" role="main">
       <div className="hero-content">
-      <img src={liteLogo} />
-      <div>
-        <h2>React Lite UI</h2><br/>
-        <p>A set of light wright React Components</p>
+        <img src={liteLogo} alt="react-lite-ui-logo" />
+        <div>
+          <h2>React Lite UI</h2><br />
+          <p>A set of light wright React Components</p>
+        </div>
       </div>
-      </div>
-    <div className="hero-bottom"></div>
+      <div className="hero-bottom" />
     </div>
+    <LandingMain />
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
---
+++ doc/src/templates/landing-main.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class LandingMain extends Component {
+  render() {
+    return (
+      <div>
+        landing main
+      </div>
+    )
+  }
+}
+
+export default LandingMain;
