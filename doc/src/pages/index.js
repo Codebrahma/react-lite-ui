548b0d21ee0b0e1be32fd1d4d1b044d1d5ced4fd
rishichawda <chawda.rishii@gmail.com>
2018-11-28 17:19:52 +0530

    💄  (hero)-add bottom transition on scroll
     #123

--- doc/src/pages/index.js
+++ doc/src/pages/index.js
@@ -1,27 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Layout from '../components/layout';
 import './index.scss';
 import liteLogo from '../images/match.svg';
 import LandingMain from '../templates/landing-main';
 
-const registerScroll = (e) => {
-  console.log(e);
-};
+class IndexPage extends Component {
+  constructor(props) {
+    super(props);
+    this.heroBottom = null;
+  }
+  componentDidMount() {
+    window.addEventListener('scroll', this.registerScroll);
+  }
 
-const IndexPage = () => (
-  <Layout>
-    <div onScroll={registerScroll} className="hero-container" role="main">
-      <div className="hero-content">
-        <img src={liteLogo} alt="react-lite-ui-logo" />
-        <div>
-          <h2>React Lite UI</h2><br />
-          <p>A set of light wright React Components</p>
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.registerScroll);
+  }
+
+  registerScroll = (e) => {
+    const skewValue = 6 - ((6 / (0.8 * e.path[1].innerHeight)) * (e.path[1].pageYOffset || 1));
+    document.getElementsByClassName('hero-bottom')[0].style.transform = `skewY(${-(skewValue)}deg)`;
+  }
+
+  render() {
+    return (
+      <Layout>
+        <div className="hero-container" role="main">
+          <div className="hero-content">
+            <img src={liteLogo} alt="react-lite-ui-logo" />
+            <div>
+              <h2>React Lite UI</h2><br />
+              <p>A set of light weight React Components</p>
+            </div>
+          </div>
+          <div
+            className="hero-bottom"
+            ref={(ref) => { this.heroBottom = ref; }}
+          />
         </div>
-      </div>
-      <div className="hero-bottom" />
-    </div>
-    <LandingMain />
-  </Layout>
-);
+        <LandingMain />
+      </Layout>
+    );
+  }
+}
 
 export default IndexPage;
--- doc/src/templates/landing-main/index.js
+++ doc/src/templates/landing-main/index.js
@@ -7,34 +7,38 @@ import quill from '../../images/quill-drawing-a-line.svg';
 
 import './index.scss';
 
-const LandingMain = () => (
-  <div className="landing-main">
-    <div className="row">
-      <div className="col">
-        <img src={toolsImage} alt="easily-customizable" />
-        <p>Easily Customizable by SASS</p>
+class LandingMain extends Component {
+  render() {
+    return (
+      <div className="landing-main">
+        <div className="row">
+          <div className="col">
+            <img src={toolsImage} alt="easily-customizable" />
+            <p>Easily Customizable by SASS</p>
+          </div>
+          <div className="col">
+            <img src={paintBoard} alt="easily-themeable" />
+            <p>Easily Themable</p>
+          </div>
+          <div className="col">
+            <img src={chemistry} alt="well-tested" />
+            <p>Well Tested Code</p>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <img src={codeImage} alt="components" />
+            <h2>30+</h2>
+            <p>components </p>
+          </div>
+          <div className="col">
+            <img src={quill} alt="lite-weight" />
+            <p>Extremely Lite</p>
+          </div>
+        </div>
       </div>
-      <div className="col">
-        <img src={paintBoard} alt="easily-themeable" />
-        <p>Easily Themable</p>
-      </div>
-      <div className="col">
-        <img src={chemistry} alt="well-tested" />
-        <p>Well Tested Code</p>
-      </div>
-    </div>
-    <div className="row">
-      <div className="col">
-        <img src={codeImage} alt="components" />
-        <h2>30+</h2>
-        <p>components </p>
-      </div>
-      <div className="col">
-        <img src={quill} alt="lite-weight" />
-        <p>Extremely Lite</p>
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+} 
 
 export default LandingMain;
