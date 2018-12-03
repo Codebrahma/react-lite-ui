6c11b659c37edbead01c5e49e6a6eed9fede8223
rishichawda <chawda.rishii@gmail.com>
2018-12-03 15:21:15 +0530

    🎨  (src/navbar)-improvement on navbar usage
    With the old navbar component, user was expected to provide different sets of component for mobile and desktop view which is changed in this commit. Now the navlinks are provided as children and are rendered appropriately according to screen size.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import Button from './button';
 import Card from './card';
 import Checkbox from './checkbox';
@@ -56,36 +56,18 @@ export default {
   Table,
 };
 
-// const MenuIcon = () => (
-//   <svg xmlns="http://www.w3.org/2000/svg" height="30px" id="Layer_1" version="1.1" viewBox="0 0 32 32" width="30px"><path d="M4,10h24c1.104,0,2-0.896,2-2s-0.896-2-2-2H4C2.896,6,2,6.896,2,8S2.896,10,4,10z M28,14H4c-1.104,0-2,0.896-2,2  s0.896,2,2,2h24c1.104,0,2-0.896,2-2S29.104,14,28,14z M28,22H4c-1.104,0-2,0.896-2,2s0.896,2,2,2h24c1.104,0,2-0.896,2-2  S29.104,22,28,22z" /></svg>
-// );
 
-// const NavbarMenu = () => (
-//   <div className="navlinks">
-//     <span>home</span>
-//     <span>dashboard</span>
-//     <span>contact</span>
-//   </div>
-// );
+const NavbarMenu = () => (
+  <div className="navlinks">
+    <span>home</span>
+    <span>dashboard</span>
+    <span>contact</span>
+  </div>
+);
 
-// const DesktopView = () => (
-//   <div className="navbarMenu">
-//     { <NavbarMenu /> }
-//   </div>
-// );
-
-// const MobileView = () => (
-//   <div className="navbarMobileMenu">
-//     <Popover content={<NavbarMenu />} position="bottomRight" noAction>
-//       <span className="icon-menu" />
-//     </Popover>
-//   </div>
-// );
-
-// ReactDOM.render(
-//   <Navbar leftIcon={<MenuIcon />} title="logo">
-//     <DesktopView />
-//     <MobileView />
-//   </Navbar>,
-//   document.getElementById('index'),
-// );
+ReactDOM.render(
+  <Navbar title="logo">
+    <NavbarMenu />
+  </Navbar>,
+  document.getElementById('index'),
+);
--- src/navbar/index.js
+++ src/navbar/index.js
@@ -4,69 +4,105 @@ import classnames from 'classnames';
 import { themr } from 'react-css-themr';
 import defaultTheme from './theme.scss';
 
-const Navbar = ({
-  theme,
-  position,
-  color,
-  title,
-  leftIcon,
-  rightIcon,
-  onLeftIconClick,
-  onRightIconClick,
-  flat,
-  children,
-  className,
-  ...other
-}) => {
-  const classes = classnames(
-    className,
-    theme.navbarWrapper,
-    theme[flat ? 'flat' : ''],
-    theme[`${position}Position`],
-    theme[`${color}Color`],
-  );
+class Navbar extends React.Component {
+  renderLeftContent = () => {
+    const { title, theme } = this.props;
 
-  /* eslint-disable jsx-a11y/click-events-have-key-events */
-  /* eslint-disable jsx-a11y/no-static-element-interactions */
-
-  const renderLeftContent = () => title && (
-    <div className={theme.leftContent}>
-      {
-        typeof title === 'string'
-        ? <span className={theme.title}>{title}</span>
-        : title
-      }
-    </div>
-  );
-
-  const renderRightContent = () => (
-    <div className={theme.rightContent}>
-      { children }
-    </div>
-  );
-
-  return (
-    <div className={classes} {...other}>
-      <div className={theme.innerWrapper}>
-        { leftIcon && (
-          <div className={theme.leftIcon} onClick={onLeftIconClick}>
-            {React.isValidElement(leftIcon) ? leftIcon : <img src={leftIcon} alt="navbar-left-logo" />}
-          </div>)
-        }
-        <div className={theme.navbarContent}>
-          {renderLeftContent()}
-          {renderRightContent()}
+    return (
+      title && (
+        <div className={theme.leftContent}>
+          {
+            typeof title === 'string'
+              ? <span className={theme.title}>{title}</span>
+              : title
+          }
         </div>
-        { rightIcon && (
-          <div className={theme.rightIcon} onClick={onRightIconClick}>
-            {React.isValidElement(rightIcon) ? rightIcon : <img src={rightIcon} alt="navbar-right-logo" />}
-          </div>)
-        }
+      )
+    );
+  }
+
+  renderDesktopView = () => {
+    const { children } = this.props;
+
+    return (
+      <div className="navbarMenu">
+        {children}
       </div>
-      <div className="clearfix" />
-    </div>
-  );
-};
+    );
+  }
+
+  renderMobileView = () => {
+    const { children } = this.props;
+
+    return (
+      <div className="navbarMobileMenu">
+        {children}
+        <span className="icon-menu" />
+      </div>
+    );
+  }
+
+  renderRightContent = () => {
+    const { theme } = this.props;
+
+    return (
+      <div className={theme.rightContent}>
+        {this.renderDesktopView()}
+        {this.renderMobileView()}
+      </div>
+    );
+  }
+
+  render() {
+    const {
+      theme,
+      position,
+      color,
+      title,
+      leftIcon,
+      rightIcon,
+      onLeftIconClick,
+      onRightIconClick,
+      flat,
+      children,
+      className,
+      ...other
+    } = this.props;
+
+    const classes = classnames(
+      className,
+      theme.navbarWrapper,
+      theme[flat ? 'flat' : ''],
+      theme[`${position}Position`],
+      theme[`${color}Color`],
+    );
+
+    /* eslint-disable jsx-a11y/click-events-have-key-events */
+    /* eslint-disable jsx-a11y/no-static-element-interactions */
+
+    return (
+      <div className={classes} {...other}>
+        <div className={theme.innerWrapper}>
+          {leftIcon && (
+            <div className={theme.leftIcon} onClick={onLeftIconClick}>
+              {React.isValidElement(leftIcon) ? leftIcon : <img src={leftIcon} alt="navbar-left-logo" />}
+            </div>)
+          }
+          <div className={theme.navbarContent}>
+            {this.renderLeftContent()}
+            {this.renderRightContent()}
+          </div>
+          {rightIcon && (
+            <div className={theme.rightIcon} onClick={onRightIconClick}>
+              {React.isValidElement(rightIcon) ? rightIcon : <img src={rightIcon} alt="navbar-right-logo" />}
+            </div>)
+          }
+        </div>
+        <div className="clearfix" />
+      </div>
+    );
+  }
+}
 
 Navbar.propTypes = {
   position: PropTyes.oneOf(['fixed', 'absolute', 'sticky', 'static', 'relative']),
@@ -91,8 +127,8 @@ Navbar.defaultProps = {
   title: null,
   leftIcon: null,
   rightIcon: null,
-  onLeftIconClick: () => {},
-  onRightIconClick: () => {},
+  onLeftIconClick: () => { },
+  onRightIconClick: () => { },
   flat: false,
 };
 
