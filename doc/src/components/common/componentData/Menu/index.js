d0221d0aa0cda76a3e0b01102a424f0aadefa4d4
sai <kskprasad96@gmail.com>
2018-12-07 18:40:07 +0530

    💄 Add custom menu and usage example to usage page

--- doc/src/components/common/componentData/Menu/index.js
+++ doc/src/components/common/componentData/Menu/index.js
@@ -108,11 +108,88 @@ export const componentData = {
             </Menu>
               <span>Option 2</span>
               <span>Option 3</span>
-          </Menu>
-        </PreviewBlock>
-      </PreviewElements>
-    )
-  }
-}
-  `
+            </Menu>
+          </div>
+        )
+      }
+    }
+  `,
+  componentUsage: `
+    // Component.js
+
+    class Demo extends React.Component {
+      render() {
+        return (
+          <PreviewElements>
+            <PreviewBlock header="Default Menu Component">
+
+            {/* Default Menu Component */}
+              <Menu>
+                <span>Option 1</span>
+                
+                {/* Menu can be nested and used as sub menu and passing 'submenu' prop */}
+                <Menu submenu title="Submenu 1">
+                  <span>sub1-Option 1</span>
+                  <span>sub1-Option 2</span>
+                </Menu>
+                <Menu submenu title="Submenu 2">
+                  <span>sub2-Option1</span>
+                  <span>sub2-Option2</span>
+
+                  {/* Menu nested to multiple levels */}
+                  <Menu submenu title="Nested Submenu 1">
+                    <span>Option 1</span>
+                    <span>Option 2</span>
+                  </Menu>
+                  <Menu submenu title="Nested Submenu 2">
+                    <span>Option 1</span>
+                    <span>Option 2</span>
+                  </Menu>
+
+                </Menu>
+                <span>Option 2</span>
+                <span>Option 3</span>
+              </Menu>
+
+            </PreviewBlock>
+            <PreviewBlock header="Custom Menu Component">
+
+            {/* Customized Menu, Refer theme.scss */}
+              <Menu theme={componentTheme}>
+                <span>Custom 1</span>
+                
+                {/* Nested Submneu */}
+                <Menu submenu title="Submenu 1">
+                  <span>sub1-Custom 1</span>
+                  <span>sub1-Custom 2</span>
+                </Menu>
+                
+                <span>Custom 2</span>
+                <span>Custom 3</span>
+                {/* Nested Submenu */}
+                <Menu submenu title="Submenu 2">
+                  <span>sub2-Custom1</span>
+                  <span>sub2-Custom2</span>
+                </Menu>
+              
+              </Menu>
+            </PreviewBlock>
+          </PreviewElements>
+        )
+      }
+    }
+  `,
+  componentUsageTheme: `
+    //  theme.scss
+
+    :local(.menu) {
+      color: #144c52;
+    }
+    :local(.menuitem) {
+      &:hover {
+        background-color: rgba(20, 76, 82, 0.8);
+        color: #fff;
+      }
+    }
+  `,
 };
