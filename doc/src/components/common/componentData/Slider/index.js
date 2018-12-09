3b3782b4d329cf93532d50e68bb7f3bade5d60bd
sai <kskprasad96@gmail.com>
2018-12-09 17:51:23 +0530

    💄 Update Slider Component usage content with custom examples

--- doc/src/components/common/componentData/Slider/index.js
+++ doc/src/components/common/componentData/Slider/index.js
@@ -29,4 +29,63 @@ export const componentData = {
   basicComponent: `
     <Slider />
   `,
+  componentUsage: `
+    //  Component.js
+
+    class Demo extends React.Component {
+      render() {
+        return (
+          <div>
+            <PreviewElements>
+              <PreviewBlock header="Default Slider">
+
+              {/* Default slider */}
+                <Slider />
+
+              </PreviewBlock>
+              <PreviewBlock header="range slider">
+
+              {/* Range slider with user set min and max values */}
+                <Slider range min={0} max={50}/>
+
+              </PreviewBlock>
+            </PreviewElements>
+            <PreviewElements>
+            <PreviewBlock header="custom slider">
+
+            {/* Custom Slider, Refer theme.scss */}
+              <Slider theme={componentTheme} />
+
+            </PreviewBlock>
+            <PreviewBlock header="disabled Slider">
+
+            {/* disabled slider */}
+              <Slider disabled />
+
+            </PreviewBlock>
+          </PreviewElements>
+        </div>
+        )
+      }
+    }
+  `,
+  componentUsageTheme: `
+    //  theme.scss
+
+    :local(.slider-button) {
+      background-color: #23a393;
+    }
+    
+    :local(.slider-tracker) {
+      background-color: #20716a;
+    }
+    
+    :local(.tooltip) {
+      background-color: #ffc0c2;
+      color: #20716a;
+      div {
+        border-top-color: #ffc0c2;
+      }
+    }
+  `,
 };
