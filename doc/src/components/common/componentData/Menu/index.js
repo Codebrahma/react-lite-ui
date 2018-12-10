c09ad175c61179d864eecc378ded1f9472f8a094
sai <kskprasad96@gmail.com>
2018-12-10 13:24:04 +0530

    ♻️ refactor component data for menu and remove extra import for themes

--- doc/src/components/common/componentData/Menu/index.js
+++ doc/src/components/common/componentData/Menu/index.js
@@ -57,55 +57,25 @@ export const componentData = {
   class Demo extends React.Component {
     render() {
         return (
-          <PreviewElements>
-
-            {/* Default Menu Component */}
-            <PreviewBlock header="Default Menu Component">
-              <Menu>
-                <span>Option 1</span>
-
-                {/* Menu can be nested and used as sub menu and passing 'submenu' prop */}
-                <Menu submenu title="Submenu 1">
-                  <span>sub1-Option 1</span>
-                  <span>sub1-Option 2</span>
+          <div>
+            <Menu>
+              <span>Option 1</span>
+              <Menu submenu title="Submenu 1">
+                <span>sub1-Option 1</span>
+                <span>sub1-Option 2</span>
+              </Menu>
+              <Menu submenu title="Submenu 2">
+                <span>sub2-Option1</span>
+                <span>sub2-Option2</span>
+                <Menu submenu title="Nested Submenu 1">
+                  <span>Option 1</span>
+                  <span>Option 2</span>
                 </Menu>
-                <Menu submenu title="Submenu 2">
-                  <span>sub2-Option1</span>
-                  <span>sub2-Option2</span>
-                  <Menu submenu title="Nested Submenu 1">
-                    <span>Option 1</span>
-                    <span>Option 2</span>
-                  </Menu>
-                  <Menu submenu title="Nested Submenu 2">
-                    <span>Option 1</span>
-                    <span>Option 2</span>
-                  </Menu>
-                  </Menu>
-                <span>Option 2</span>
-                <span>Option 3</span>
+                <Menu submenu title="Nested Submenu 2">
+                  <span>Option 1</span>
+                  <span>Option 2</span>
+                </Menu>
               </Menu>
-            </PreviewBlock>
-          <PreviewBlock header="Custom Menu Component">
-
-          {/* Customized Menu, Refer theme.scss */}
-          <Menu theme={componentTheme}>
-            <span>Option 1</span>
-            <Menu submenu title="Submenu 1">
-              <span>sub1-Option 1</span>
-              <span>sub1-Option 2</span>
-            </Menu>
-            <Menu submenu title="Submenu 2">
-              <span>sub2-Option1</span>
-              <span>sub2-Option2</span>
-              <Menu submenu title="Nested Submenu 1">
-                <span>Option 1</span>
-                <span>Option 2</span>
-              </Menu>
-              <Menu submenu title="Nested Submenu 2">
-                <span>Option 1</span>
-                <span>Option 2</span>
-              </Menu>
-            </Menu>
               <span>Option 2</span>
               <span>Option 3</span>
             </Menu>
@@ -183,13 +153,22 @@ export const componentData = {
     //  theme.scss
 
     :local(.menu) {
-      color: #144c52;
+      color: #8b79da;
     }
+    
     :local(.menuitem) {
       &:hover {
-        background-color: rgba(20, 76, 82, 0.8);
-        color: #fff;
+          background-color: #b3abd69c;
+          color: white;
+  
+        :local(.arrow) {
+            border-color: white;
+        }
       }
+    }
+    
+    :local(.arrow) {
+      border-color: #8b79da;
     }
   `,
 };
