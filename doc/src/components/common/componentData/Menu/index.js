dbd08d9ca5230d928192ac672f450654f5d93b69
rishichawda <chawda.rishii@gmail.com>
2018-12-10 12:35:32 +0530

    🐛  (menu)-fix bug on theme not getting applied to submenu
     #203

--- doc/src/components/common/componentData/Menu/index.js
+++ doc/src/components/common/componentData/Menu/index.js
@@ -54,34 +54,65 @@ export const componentData = {
     },
   ],
   basicComponent: `
-    class Demo extends React.Component {
-      render() {
+  class Demo extends React.Component {
+    render() {
         return (
-          <div>
-            <Menu>
-              <span>Option 1</span>
-              <Menu submenu title="Submenu 1">
+          <PreviewElements>
+
+            {/* Default Menu Component */}
+            <PreviewBlock header="Default Menu Component">
+              <Menu>
+                <span>Option 1</span>
+
+                {/* Menu can be nested and used as sub menu and passing 'submenu' prop */}
+                <Menu submenu title="Submenu 1">
                   <span>sub1-Option 1</span>
                   <span>sub1-Option 2</span>
                 </Menu>
-              <Menu submenu title="Submenu 2">
-                <span>sub2-Option1</span>
-                <span>sub2-Option2</span>
-                <Menu submenu title="Nested Submenu 1">
-                  <span>Option 1</span>
-                  <span>Option 2</span>
-                </Menu>
-                <Menu submenu title="Nested Submenu 2">
-                  <span>Option 1</span>
-                  <span>Option 2</span>
-                </Menu>
+                <Menu submenu title="Submenu 2">
+                  <span>sub2-Option1</span>
+                  <span>sub2-Option2</span>
+                  <Menu submenu title="Nested Submenu 1">
+                    <span>Option 1</span>
+                    <span>Option 2</span>
+                  </Menu>
+                  <Menu submenu title="Nested Submenu 2">
+                    <span>Option 1</span>
+                    <span>Option 2</span>
+                  </Menu>
+                  </Menu>
+                <span>Option 2</span>
+                <span>Option 3</span>
               </Menu>
+            </PreviewBlock>
+          <PreviewBlock header="Custom Menu Component">
+
+          {/* Customized Menu, Refer theme.scss */}
+          <Menu theme={componentTheme}>
+            <span>Option 1</span>
+            <Menu submenu title="Submenu 1">
+              <span>sub1-Option 1</span>
+              <span>sub1-Option 2</span>
+            </Menu>
+            <Menu submenu title="Submenu 2">
+              <span>sub2-Option1</span>
+              <span>sub2-Option2</span>
+              <Menu submenu title="Nested Submenu 1">
+                <span>Option 1</span>
+                <span>Option 2</span>
+              </Menu>
+              <Menu submenu title="Nested Submenu 2">
+                <span>Option 1</span>
+                <span>Option 2</span>
+              </Menu>
+            </Menu>
               <span>Option 2</span>
               <span>Option 3</span>
-            </Menu>
-          </div>
-        )
-      }
-    }
+          </Menu>
+        </PreviewBlock>
+      </PreviewElements>
+    )
+  }
+}
   `
 };
