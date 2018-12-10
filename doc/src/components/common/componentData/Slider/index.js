121722d49d400d9a722af12e62f4cd58f34c73dd
sai <kskprasad96@gmail.com>
2018-12-10 12:01:02 +0530

    📝 Add readMe and docs for slider component

--- doc/src/components/common/componentData/Slider/index.js
+++ doc/src/components/common/componentData/Slider/index.js
@@ -1,29 +1,67 @@
 export const componentData = {
   htmlStructure: `
-    <div>
-      <!-- HTML Content will be added after merging PR #150 -->
+    <div class="slider">
+      <div class="slider-tracker" style="width: 14.1402%;">
+        <span class="slider-button"></span>
+        <div class="tooltip">
+          <span>14</span>
+          <div></div>
+        </div>
+      </div>
     </div>
   `,
   propsData: [
     {
-      prop: 'Slider  prop',
-      defaultValue: 'default prop Slider ',
-      description: 'I am Slider ing default description for component',
+      prop: 'range',
+      type: 'Boolean',
+      defaultValue: 'false',
+      description: 'Prop to render range slider.',
     },
     {
-      prop: 'Slider  some prop',
-      defaultValue: 'some default prop Slider ',
-      description: 'I am some Slider ing default description for component',
+      prop: 'disabled',
+      type: 'Boolean',
+      defaultValue: 'false',
+      description: 'Disables the slider.',
+    },
+    {
+      prop: 'min',
+      type: 'Number',
+      defaultValue: '0',
+      description: 'Prop to set the minimum value of slider.',
+    },
+    {
+      prop: 'max',
+      type: 'Number',
+      defaultValue: '100',
+      description: 'Prop to set the maximum value of slider.',
+    },
+    {
+      prop: 'onChange',
+      type: 'Function',
+      defaultValue: '',
+      description: 'The callback function to be called when slider value changes, returns min and max values.',
     },
   ],  
   themesData: [
     {
-      name: 'default theme',
-      description: 'Slider ing description',
+      name: 'slider',
+      description: 'Class used for the root element.',
     },
     {
-      name: 'default some theme',
-      description: 'Slider ing some description',
+      name: 'slider-button',
+      description: 'Class used for slider button element.',
+    },
+    {
+      name: 'slider-tracker',
+      description: 'Class used for slider tracking element.',
+    },
+    {
+      name: 'slider-offset',
+      description: 'Class used for slider offset element, when using range slider.',
+    },
+    {
+      name: 'tooltip',
+      description: 'Class used for slider tooltip element.',
     },
   ],
   basicComponent: `
