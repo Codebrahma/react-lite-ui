a2045b8106182383adfd52b14a07f85240d1c400
rishichawda <chawda.rishii@gmail.com>
2018-12-13 17:43:35 +0530

    🏗  (webpack)-configure builds

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,18 +1,28 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const path = require('path');
+const glob = require('glob');
+
+function getFiles(filePattern) {
+  const files = {
+    'index.js': path.join(__dirname, 'lib/index.js'),
+  };
+  glob.sync(filePattern).forEach((file) => {
+    files[file.replace('lib/', '../components/')] = path.join(__dirname, file);
+  });
+  return files;
+}
 
 module.exports = merge(common, {
   devtool: 'source-map',
   mode: 'production',
+  entry: getFiles('lib/**/index.js'),
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js',
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name]',
     libraryTarget: 'umd',
   },
-  entry: path.resolve(__dirname, 'lib'),
   externals: [
-    // eslint-disable-next-line quote-props
     'react',
     'react-dom',
   ],
