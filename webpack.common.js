21b36a4f3d4e4506bdaa27cbc6aac49293fcbca2
rishichawda <chawda.rishii@gmail.com>
2018-12-13 11:45:41 +0530

    🔧  (root)-update paths

--- webpack.common.js
+++ webpack.common.js
@@ -1,12 +1,4 @@
 const path = require('path');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
-// const devMode = process.env.NODE_ENV !== 'production';
-const htmlWebpackPlugin = new HtmlWebPackPlugin({
-  template: './src/index.html',
-  filename: './index.html',
-});
 
 module.exports = {
   module: {
@@ -33,7 +25,7 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        include: path.resolve(__dirname, './src'),
+        include: path.resolve(__dirname, './lib'),
         use: [
           'style-loader',
           `${require.resolve('css-loader')}?sourceMap&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]`,
@@ -46,12 +38,4 @@ module.exports = {
       },
     ],
   },
-  plugins: [
-    htmlWebpackPlugin,
-    // new BundleAnalyzerPlugin(),
-    // new MiniCssExtractPlugin({
-    //   filename: devMode ? '[name].css' : '[name].[hash].css',
-    //   chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
-    // }),
-  ],
 };
