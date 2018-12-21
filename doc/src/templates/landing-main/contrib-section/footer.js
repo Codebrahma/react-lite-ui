27a4a48442afd962fa7148eeae6702f1877802fd
rishichawda <chawda.rishii@gmail.com>
2018-12-21 17:26:45 +0530

    💄  (footer)-minor changes on footer
     #253

--- doc/src/templates/landing-main/contrib-section/footer.js
+++ doc/src/templates/landing-main/contrib-section/footer.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Button from '../../../../../lib/button';
+import theme from './footer.module.scss';
 
 const FooterSection = () => (
   <div className="row">
     <div className="row">
       {'Want us to build something for you? '}
       {'Great! We would love to!'}
-      <Button href="https://codebrahma.com/contact/" flat >Let us know here!</Button>
+      <Button theme={theme} bordered href="https://codebrahma.com/contact/" >Let us know here!</Button>
     </div>
-    <div className="bottom-bar">Made with <span className="heart">❤</span> by<a target="blank" href="https://www.codebrahma.com"><span>Codebrahma</span></a></div>
+    <div className="row bottom-bar">Made with <span className="heart">❤</span> by<a target="blank" href="https://www.codebrahma.com"><span>Codebrahma</span></a></div>
   </div>
 );
 
