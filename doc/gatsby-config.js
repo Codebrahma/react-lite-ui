e5ff0783c4244431928e5edeec5497bd5342f586
rishichawda <chawda.rishii@gmail.com>
2018-12-26 15:36:14 +0530

    feat(doc): add google analytics to page

--- doc/gatsby-config.js
+++ doc/gatsby-config.js
@@ -26,5 +26,11 @@ module.exports = {
       },
     },
     'gatsby-plugin-offline',
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'UA-41862404-1',
+      },
+    },
   ],
 };
