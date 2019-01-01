de21936f3689da0ff2473e6d4dc86ee4f7284545
Rishi Kumar Chawda <rishichawda@users.noreply.github.com>
2019-01-01 18:30:35 +0530

    fix(navbar): update navbar design (#297)
    * fix(navbar): navbar logo fix
    
    * fix(navbar): fix inner wrapper and children overflows and adjust spacing
    
    #287
    
    * fix(navbar): fix navbar spacing issues and right icon color

--- doc/src/components/common/componentData/Navbar/index.js
+++ doc/src/components/common/componentData/Navbar/index.js
@@ -145,10 +145,6 @@ export const componentData = {
 
       render() {
 
-        const MenuIcon = () => (
-          <svg xmlns="http://www.w3.org/2000/svg" height="30px" id="Layer_1"  version="1.1" viewBox="0 0 30 30" width="30px"><path d="M4,10h24c1.104,0,2-0.896,2-2s-0.896-2-2-2H4C2.896,6,2,6.896,2,8S2.896,10,4,10z M28,14H4c-1.104,0-2,0.896-2,2  s0.896,2,2,2h24c1.104,0,2-0.896,2-2S29.104,14,28,14z M28,22H4c-1.104,0-2,0.896-2,2s0.896,2,2,2h24c1.104,0,2-0.896,2-2  S29.104,22,28,22z"/></svg>
-        );
-
         return (
           <div>
             <PreviewBlock header="Default Navbar">
@@ -160,7 +156,7 @@ export const componentData = {
             <PreviewBlock header="Navbar with leftIcon">
 
             {/* Navbar with icon on left set with leftIcon prop */}
-              <Navbar leftIcon={<MenuIcon />} title="logo" />
+              <Navbar leftIcon={match} title="logo" />
 
             </PreviewBlock>
             <PreviewBlock header="Flat Navbar (No box shadow)">
