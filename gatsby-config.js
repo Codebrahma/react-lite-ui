6466876cafb04379d8840167de30412a80d0c193
Rishi Kumar Chawda <rishichawda@users.noreply.github.com>
2019-01-04 16:03:22 +0530

    feat(root): add gatsby plugin for gtag (#318)

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,5 +32,16 @@ module.exports = {
       },
     },
     'gatsby-plugin-offline',
+    {
+      resolve: 'gatsby-plugin-gtag',
+      options: {
+        // your google analytics tracking id
+        trackingId: 'UA-41862404-1',
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // enable ip anonymization
+        anonymize: true,
+      },
+    },
   ],
 };
