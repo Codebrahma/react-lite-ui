daa0985745ffc7759b4c2b92a0270228ce7a1d32
errorr404 <dkbishwash@gmail.com>
2019-04-09 19:19:20 +0530

    changed items

--- src/components/common/componentData/Accordion/index.js
+++ src/components/common/componentData/Accordion/index.js
@@ -108,8 +108,8 @@ export const componentData = {
       class Demo extends React.Component {
         render() {
             const items = [
-              {header:'Lorem Ipsum is simply dummy text of ',content:'Lorem Ipsum&nbsp;is simply dummy text of the printing and typesetting industry'},
-              {header:'printing and typesetting industry',content:'Lorem Ipsum&nbsp;is simply dummy text of the printing and typesetting industry'},
+              {header:'Lorem Ipsum is simply dummy text of ',content:'Lorem Ipsum&nbsp;is simply dummy text of the printing and typesetting industry',key:'1'},
+              {header:'printing and typesetting industry',content:'Lorem Ipsum&nbsp;is simply dummy text of the printing and typesetting industry',key:'2'},
         ]
           return (
             <div>
@@ -123,8 +123,8 @@ export const componentData = {
     class Demo extends React.Component {
       render(){
          const items = [
-          {header:'Lorem Ipsum is simply dummy text of ',content:'Lorem Ipsum&nbsp;is simply dummy text of the printing and typesetting industry'},
-          {header:'printing and typesetting industry',content:'Lorem Ipsum&nbsp;is simply dummy text of the printing and typesetting industry'},
+          {header:'Lorem Ipsum is simply dummy text of ',content:'Lorem Ipsum&nbsp;is simply dummy text of the printing and typesetting industry',key:'1'},
+          {header:'printing and typesetting industry',content:'Lorem Ipsum&nbsp;is simply dummy text of the printing and typesetting industry',key:'2'},
          ];
        return (
          <div>
