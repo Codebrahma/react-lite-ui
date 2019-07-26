7bae5428180fa8f769cfe23a3e0544c291cf2a2a
rishichawda <chawda.rishii@gmail.com>
2019-07-26 13:07:30 +0530

    build(dev): greatly improves rebuild times
    incremental builds took a longer time that greatly effected developer experience, this commit
    improves the rebuild time and generates outputs almost instantaneously.

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,11 @@ const common = require('./webpack.common.js');
 const path = require('path');
 
 module.exports = merge(common, {
-   devtool: 'inline-source-map',
-   mode: 'development',
-   watch: true,
-   watchOptions: {
-      aggregateTimeout: 1000,
-      ignored: ['node_modules', 'test'],
-   },
+  devtool: "cheap-module-eval-source-map",
+  mode: "development",
+  watch: true,
+  watchOptions: {
+    aggregateTimeout: 1000,
+    ignored: ["node_modules", "test"]
+  }
 });
