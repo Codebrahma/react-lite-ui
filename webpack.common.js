676a95ca2e6fe15e7cd0c8dd28777f0677da0820
Rishi Kumar Chawda <rishichawda@users.noreply.github.com>
2019-07-26 13:44:12 +0530

    build(dev): greatly improves rebuild times (#338)
    incremental builds took a longer time that greatly effected developer experience, this commit improves the rebuild time and generates outputs almost instantaneously.

--- webpack.common.js
+++ webpack.common.js
@@ -41,7 +41,7 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        exclude: /node_modules/,
+        exclude: path.resolve(__dirname, 'node_modules'),
         use: {
           loader: 'babel-loader',
         },
